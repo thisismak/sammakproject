5faa6560ab6a2477c456f1456b8361a933ccc8eb
Sam Mak <thismywing@hotmail.com>
2025-02-02 17:58:48 +0800

    1

--- typescript-practice/Testing04.ts
+++ typescript-practice/Testing04.ts
@@ -1,7 +1,6 @@
 // 以 2 為間距顯示由 1 至 9 的數字，即顯示 1、3、5、7、9
-for (let i = 1 ; i < 11 ; i ++) {
-    if (i % 2 != 0) {
-        console.log(i)
-        continue
-    }
+for (let i = 0 ; i < 11 ; i ++) {
+    i ++
+    i ++
+    console.log(i)
 }
