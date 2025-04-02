8ee15540c70a12f66aef155ea6d6f584fe32f919
Sam Mak <thismywing@hotmail.com>
2025-04-02 23:52:38 +0800

    update_20250402_2352

---
+++ android/app/src/main/java/com/sammak/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sammak.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sammak.app',
+  appName: 'sammakproject',
+  webDir: 'www'
+};
+
+export default config;
